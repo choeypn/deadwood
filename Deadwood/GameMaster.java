7495dc427690d779217600913e0d994577efb8cb
choeypn <choeypn@wwu.edu>
2019-07-10 14:19:03 -0700

    GameMaster Class

---
+++ Deadwood/GameMaster.java
@@ -0,0 +1,18 @@
+package Deadwood;
+
+public class GameMaster {
+	//awaiting Set class
+	
+	//fields
+	private int shot_counters;
+	private int game_day;
+	
+	//methods
+	public GameMaster gameMaster() {return null;}
+	public void upgradePlayer(Player p) {}
+	public void gameInit(Player[] ps) {}
+	public void payPlayer(Player p) {}
+	public void endDay(Player[] ps) {}
+	public void endGame(Player[] ps) {}
+	//public void wrapSet(Set s){}
+}
