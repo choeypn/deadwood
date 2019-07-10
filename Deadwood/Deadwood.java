51c3006261e475dfc95af54d8812d5c8a24f5d5f
Vlad <bugayev@wwu.edu>
2019-07-10 15:21:27 -0700

    Added templates of other class files - will add skeleton methods and variables soon

---
+++ Deadwood/Deadwood.java
@@ -0,0 +1,16 @@
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
+//                                DEADWOOD                                  //
+//  Programmers: Vlad Bugayev, Natthapong Choeypant                         //
+//  Class: CSCI 345, Summer 2019                                            //
+//                                                                          //
+//  This program is our design of the Deadwood board game.                  //
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
+public class Deadwood {
+
+    // Main Method - program entry point
+    public static void main(String[] argv) {
+
+        System.out.println("yoooo");
+
+    }
+}
