44abbfe1995cd6a399cfe9ce7008a43f6c8a1f6f
choeypn <choeypn@wwu.edu>
2019-07-10 14:12:57 -0700

    Role Class

---
+++ Role.java
@@ -0,0 +1,13 @@
+package Deadwood;
+
+public class Role {
+	
+	//fields
+	private boolean starring;
+	private int rank;
+	private boolean available;
+	
+	//method
+	public Role takeRole(boolean starring,int rank) {return null;}
+	
+}
