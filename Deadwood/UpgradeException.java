68522529fe8302732f2767230a7f024164fb1abb
Natthapong None Choeypant <choeypn@LabImage2016-17>
2019-07-19 15:18:12 -0700

    added upgrade excepton

---
+++ Deadwood/UpgradeException.java
@@ -0,0 +1,17 @@
+package Deadwood;
+
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
+//                DEADWOOD - ROLE EXCEPTION CLASS                       //
+//  Programmers: Vlad Bugayev, Natthapong Choeypant                         //
+//  Class: CSCI 345, Summer 2019                                            //
+//                                                                          //
+//                                                                          //
+// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~ //
+
+// Define an upgrade exception - occurs when a player failed to upgrade the rank
+public class UpgradeException extends Exception {
+
+    public UpgradeException(){
+        super("Invalid Upgrade");
+    }
+}
