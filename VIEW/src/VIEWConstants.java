8d088cc0f8300cd1c474df698decc0c69801da6b
Vlad <bugayev@wwu.edu>
2019-07-27 17:42:02 -0700

    easy comment for changing constants

--- VIEW/src/VIEWConstants.java
+++ VIEW/src/VIEWConstants.java
@@ -2,37 +2,40 @@ package VIEW.src;
 
 public class VIEWConstants {
     public static final String DEADWOOD_TITLE = "Deadwood";
+
+
     public static final String GAME_BOARD_IMAGE = "src/board.jpg";
     public static final String[] CARDS_IMAGE = {"src/cards/01.png","src/cards/02.png",
-    											"src/cards/03.png","src/cards/04.png",
-    											"src/cards/05.png","src/cards/06.png",
-    											"src/cards/07.png","src/cards/08.png",
-    											"src/cards/09.png","src/cards/10.png",
-    											"src/cards/11.png","src/cards/12.png",
-    											"src/cards/13.png","src/cards/14.png",
-    											"src/cards/15.png","src/cards/16.png",
-    											"src/cards/17.png","src/cards/18.png",
-    											"src/cards/19.png","src/cards/20.png",
-    											"src/cards/21.png","src/cards/22.png",
-    											"src/cards/23.png","src/cards/24.png",
-    											"src/cards/25.png","src/cards/26.png",
-    											"src/cards/27.png","src/cards/28.png",
-    											"src/cards/29.png","src/cards/30.png",
-    											"src/cards/31.png","src/cards/32.png",
-    											"src/cards/33.png","src/cards/34.png",
-    											"src/cards/35.png","src/cards/36.png",
-    											"src/cards/37.png","src/cards/38.png",
-    											"src/cards/39.png","src/cards/40.png"};
+                                                "src/cards/03.png","src/cards/04.png",
+                                                "src/cards/05.png","src/cards/06.png",
+                                                "src/cards/07.png","src/cards/08.png",
+                                                "src/cards/09.png","src/cards/10.png",
+                                                "src/cards/11.png","src/cards/12.png",
+                                                "src/cards/13.png","src/cards/14.png",
+                                                "src/cards/15.png","src/cards/16.png",
+                                                "src/cards/17.png","src/cards/18.png",
+                                                "src/cards/19.png","src/cards/20.png",
+                                                "src/cards/21.png","src/cards/22.png",
+                                                "src/cards/23.png","src/cards/24.png",
+                                                "src/cards/25.png","src/cards/26.png",
+                                                "src/cards/27.png","src/cards/28.png",
+                                                "src/cards/29.png","src/cards/30.png",
+                                                "src/cards/31.png","src/cards/32.png",
+                                                "src/cards/33.png","src/cards/34.png",
+                                                "src/cards/35.png","src/cards/36.png",
+                                                "src/cards/37.png","src/cards/38.png",
+                                                "src/cards/39.png","src/cards/40.png"};
     public static final String[] DICE_IMAGE = {"src/dice/b1.png","src/dice/b2.png",
-    											"src/dice/b3.png","src/dice/b4.png",
-    											"src/dice/b5.png","src/dice/b6.png",
-    											"src/dice/c1.png","src/dice/c2.png",
-    											"src/dice/c3.png","src/dice/c4.png",
-    											"src/dice/c5.png","src/dice/c6.png",
-    											"src/dice/g1.png","src/dice/g2.png",
-    											"src/dice/g3.png","src/dice/g4.png",
-    											"src/dice/g5.png","src/dice/g6.png"};
-    /*
+                                                "src/dice/b3.png","src/dice/b4.png",
+                                                "src/dice/b5.png","src/dice/b6.png",
+                                                "src/dice/c1.png","src/dice/c2.png",
+                                                "src/dice/c3.png","src/dice/c4.png",
+                                                "src/dice/c5.png","src/dice/c6.png",
+                                                "src/dice/g1.png","src/dice/g2.png",
+                                                "src/dice/g3.png","src/dice/g4.png",
+                                                "src/dice/g5.png","src/dice/g6.png"};
+     
+     /*
     public static final String GAME_BOARD_IMAGE = "board.jpg";
     public static final String[] CARDS_IMAGE = {"cards/01.png","cards/02.png",
     											"cards/03.png","cards/04.png",
