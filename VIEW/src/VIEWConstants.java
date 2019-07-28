77d12069dbfe7cc29efeb80912ec5c5b67239ef8
Natthapong None Choeypant <choeypn@LabImage2016-17>
2019-07-27 17:28:30 -0700

    changed some constants

--- VIEW/src/VIEWConstants.java
+++ VIEW/src/VIEWConstants.java
@@ -32,6 +32,39 @@ public class VIEWConstants {
     											"src/dice/g1.png","src/dice/g2.png",
     											"src/dice/g3.png","src/dice/g4.png",
     											"src/dice/g5.png","src/dice/g6.png"};
+    /*
+    public static final String GAME_BOARD_IMAGE = "board.jpg";
+    public static final String[] CARDS_IMAGE = {"cards/01.png","cards/02.png",
+    											"cards/03.png","cards/04.png",
+    											"cards/05.png","cards/06.png",
+    											"cards/07.png","cards/08.png",
+    											"cards/09.png","cards/10.png",
+    											"cards/11.png","cards/12.png",
+    											"cards/13.png","cards/14.png",
+    											"cards/15.png","cards/16.png",
+    											"cards/17.png","cards/18.png",
+    											"cards/19.png","cards/20.png",
+    											"cards/21.png","cards/22.png",
+    											"cards/23.png","cards/24.png",
+    											"cards/25.png","cards/26.png",
+    											"cards/27.png","cards/28.png",
+    											"cards/29.png","cards/30.png",
+    											"cards/31.png","cards/32.png",
+    											"cards/33.png","cards/34.png",
+    											"cards/35.png","cards/36.png",
+    											"cards/37.png","cards/38.png",
+    											"cards/39.png","cards/40.png"};
+    public static final String[] DICE_IMAGE = {"dice/b1.png","dice/b2.png",
+    											"dice/b3.png","dice/b4.png",
+    											"dice/b5.png","dice/b6.png",
+    											"dice/c1.png","dice/c2.png",
+    											"dice/c3.png","dice/c4.png",
+    											"dice/c5.png","dice/c6.png",
+    											"dice/g1.png","dice/g2.png",
+    											"dice/g3.png","dice/g4.png",
+    											"dice/g5.png","dice/g6.png"};
+    */
+    
     public static final int[][] CardsCoordinates = {{0,0},{0,0},{282,28},{20,65},
     												{371,281},{631,281},{970,28},{27,732},
     												{254,476},{622,473},{622,733},{970,740}};
